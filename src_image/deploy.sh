faab1167f88fb7a49df130e4de3f69a98144ca13
GOSSIN <thomas.gossin@atos.net>
2021-05-27 14:15:09 +0200

    img

--- src_image/deploy.sh
+++ src_image/deploy.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env sh
 
-echo "Deploying Production model name=Fashion_MNISTmodel"
+#conda env config vars set MLFLOW_TRACKING_URI="sqlite:///mlruns_cnn.db" # Windows
 
-# Set enviorment variable for the tracking URL where the Model Registry is
-
-conda env config vars set MLFLOW_TRACKING_URI="sqlite:///mlruns_cnn.db" #
-
-#!/usr/bin/env sh
 export MLFLOW_TRACKING_URI=sqlite:///mlruns.db
 
 mlflow models serve --model-uri models:/Model_prod/production --no-conda
