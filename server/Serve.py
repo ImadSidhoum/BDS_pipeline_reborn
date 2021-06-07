a9686d420d047e666e410bc56f693e2da85270d6
GOSSIN <thomas.gossin@atos.net>
2021-06-07 15:42:48 +0200

    server

--- server/Serve.py
+++ server/Serve.py
@@ -1,25 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 from pydantic import BaseModel
-import tensorflow as tf
 import numpy as np
 from mlflow.tracking import MlflowClient
 import mlflow
 
+from starlette_exporter import PrometheusMiddleware, handle_metrics
+import prometheus_client as prom
+import time
 
-path=None
-mlflow.set_tracking_uri("sqlite:///mlruns.db")
-client = MlflowClient()
-for rm in client.list_registered_models():
-    if dict(rm)["name"] == "model":
-        l = dict(rm)["latest_versions"]
-        for elt in l:
-            if elt.current_stage == 'Production':
-                path = elt.source 
-print(path)
+#docker run -it  -p 5001:5001 --mount type=bind,source=$(pwd),target=/app land95/mlflow-server:0.1
 
 # Initialisation
 app = FastAPI()
+mlflow.set_tracking_uri("sqlite:///mlruns.db")
+
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
+histogram_inference_time = prom.Histogram('Inference_time', 'This is inference time')
+histogram_class_prediction = prom.Histogram('api_label_pred', 'None')
+count_class_prediction = prom.Counter('api_label_pred_number', 'None count')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://grafana:3000'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # requets
 class Item(BaseModel):
@@ -30,23 +39,36 @@ class Item(BaseModel):
 async def index():
     return "Server Up"
 
+def count_pred_label(y):
+    size_y = y.shape[-1]
+    # list_count_label = np.zeros(size_y+1 if size_y==1 else size_y)
+    class_y = np.argmax(y, axis=-1)
+    for i in range(class_y.shape[0]):
+        # list_count_label[class_y[i]] +=1
+        histogram_class_prediction.observe(class_y[i])
+    # return list_count_label.tolist()
 
-@app.post('/predict')
-async def predict(item:Item):
-    path=None
-    mlflow.set_tracking_uri("sqlite:///mlruns.db")
-    client = MlflowClient()
-    for rm in client.list_registered_models():
-        if dict(rm)["name"] == "model1":
-            l = dict(rm)["latest_versions"]
-            for elt in l:
-                if elt.current_stage == 'Production':
-                    path = elt.source 
-    if path:
-        model = tf.keras.models.load_model(path+"/data/model")
-        print(model.summary())
+@app.post('/predict/{name}')
+async def predict(name, item:Item):
+    stage = 'Production'
+    # try:
+    model = mlflow.pyfunc.load_model(model_uri=f"models:/{name}/{stage}")
+    data = np.array(item.data)
+    start_timer = time.time()
+    y = model.predict(data)
+    histogram_inference_time.observe(time.time()- start_timer)
+    count_class_prediction.inc(data.shape[0])
+    return y.tolist()
+    # except:
+    #     print('404: Model not found.')
+    #     return 404
+
+@app.post('/predict/{name}/{stage}')
+async def predict(name, stage, item:Item):
+    try:
+        model = mlflow.pyfunc.load_model(model_uri=f"models:/{name}/{stage}")
         data = np.array(item.data)
         return model.predict(data).tolist()
-    
-    else:
-        return 'model not found'
+    except:
+        print('404: Model not found.')
+        return 404
--- server/example/client_image.py
+++ server/example/client_image.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 import json
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
-'''
+
 datagen = ImageDataGenerator(rescale=1./255)
 test_generator = datagen.flow_from_directory(
-    directory= "data",
+    directory= "../src_image/data",
     target_size=(150, 150),
     color_mode="rgb",
     batch_size=32,
@@ -15,13 +15,17 @@ test_generator = datagen.flow_from_directory(
 )
 
 #print(test_generator[0][0][0:2].shape)
-'''
-images = np.random.randint(0, 1, size=(2,150, 150, 3)).tolist()
+images = test_generator[0][0][0:2].tolist()
+print(test_generator[0][0][0:2].shape)
+print(test_generator[0][1][0:2])
+
+
+# images = np.random.randint(0, 1, size=(10,150, 150, 3)).tolist()
 
 
 data = json.dumps({"signature_name": "serving_default", "data": images})
 headers = {"content-type": "application/json"}
-json_response = requests.post(f'http://127.0.0.1:5001/predict', data=data, headers=headers)
+json_response = requests.post(f'http://127.0.0.1:5001/predict/model', data=data, headers=headers)
 js = json_response.text 
 if js:
     predictions = js
